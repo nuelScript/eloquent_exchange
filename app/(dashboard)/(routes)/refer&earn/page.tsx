896aa9bc330bc8af6e4406180d6a93449fceedc7
Parseval <igwee3333@gmail.com>
2023-10-08 15:21:08 +0100

    Change Biko

--- app/(dashboard)/(routes)/refer&earn/page.tsx
+++ app/(dashboard)/(routes)/refer&earn/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getRefferalRoute } from "@/routes/route";
+import { getRefferalRoute } from "@/lib/helpers";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
