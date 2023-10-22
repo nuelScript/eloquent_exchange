ec2451c3b9641319054bd5ae2feeec4e5e460879
Parseval <igwee3333@gmail.com>
2023-10-22 19:15:42 +0100

    Fixes

--- app/(dashboard)/(routes)/dashboard/transactions/buy&sell/sell/sell_confirmation/page.tsx
+++ app/(dashboard)/(routes)/dashboard/transactions/buy&sell/sell/sell_confirmation/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
