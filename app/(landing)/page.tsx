8522e372c0a53dbcac2198fa3e010554286fe3fe
Samuel <sollysamuel12@gmail.com>
2024-01-03 18:00:46 +0100

    package.json updated

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -15,45 +15,45 @@ import React, { useEffect, useRef, useState } from "react";
 const LandingPage = () => {
   // const { boot } = useIntercom();
 
-  useEffect(() => {
-    // Intercom code snippet
-    (function () {
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === "function") {
-        ic("reattach_activator");
-        ic("update", w.intercomSettings);
-      } else {
-        var d = document;
-        var i = function () {
-          i.c(arguments);
-        };
-        i.q = [];
-        i.c = function (args) {
-          i.q.push(args);
-        };
-        w.Intercom = i;
-        var l = function () {
-          var s = d.createElement("script");
-          s.type = "text/javascript";
-          s.async = true;
-          s.src = "https://widget.intercom.io/widget/your_app_id";
-          var x = d.getElementsByTagName("script")[0];
-          x.parentNode.insertBefore(s, x);
-        };
-        if (w.attachEvent) {
-          w.attachEvent("onload", l);
-        } else {
-          w.addEventListener("load", l, false);
-        }
-      }
-    })();
+  // useEffect(() => {
+  //   // Intercom code snippet
+  //   (function () {
+  //     var w = window;
+  //     var ic = w.Intercom;
+  //     if (typeof ic === "function") {
+  //       ic("reattach_activator");
+  //       ic("update", w.intercomSettings);
+  //     } else {
+  //       var d = document;
+  //       var i = function () {
+  //         i.c(arguments);
+  //       };
+  //       i.q = [];
+  //       i.c = function (args) {
+  //         i.q.push(args);
+  //       };
+  //       w.Intercom = i;
+  //       var l = function () {
+  //         var s = d.createElement("script");
+  //         s.type = "text/javascript";
+  //         s.async = true;
+  //         s.src = "https://widget.intercom.io/widget/your_app_id";
+  //         var x = d.getElementsByTagName("script")[0];
+  //         x.parentNode.insertBefore(s, x);
+  //       };
+  //       if (w.attachEvent) {
+  //         w.attachEvent("onload", l);
+  //       } else {
+  //         w.addEventListener("load", l, false);
+  //       }
+  //     }
+  //   })();
 
-    // Call boot method
-    (window as any).Intercom("boot", {
-      app_id: "your_app_id",
-    });
-  }, []);
+  //   // Call boot method
+  //   (window as any).Intercom("boot", {
+  //     app_id: "your_app_id",
+  //   });
+  // }, []);
 
   // useEffect(() => {
   //   boot();
