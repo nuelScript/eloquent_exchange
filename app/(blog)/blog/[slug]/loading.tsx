9dbd291c603aae5ff51b584bfa3fe409505b7a4e
Parseval <igwee3333@gmail.com>
2024-03-19 02:14:33 +0100

    chore: loading state

--- app/(blog)/blog/[slug]/loading.tsx
+++ app/(blog)/blog/[slug]/loading.tsx
@@ -2,8 +2,8 @@ import { Loader } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+    <div className="w-full h-screen flex items-center justify-center">
+      <Loader className="h-20 w-20 text-[#4168B7] dark:text-[#A77700] animate-spin" />
     </div>
   );
 };
